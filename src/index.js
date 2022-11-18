6b88cc7703ab536327218614f29a301e65b6f383
LeadSeason <Pauli.tiittanen113@gmail.com>
2022-11-18 09:48:21 +0200

    mabe this will fix it idk

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ initTiltEffect();
 
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("showPhone").addEventListener("click", function (e) {
-        div = document.getElementById("contactShowData");
+        let div = document.getElementById("contactShowData");
         div.innerHTML = '<a rel="noreferrer" target="_blank" class="cta-btn cta-btn--resume">+358 404129151</a>';
     })
 }); 
