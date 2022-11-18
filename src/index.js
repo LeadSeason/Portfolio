e9c8b222b7abca36fc35977af30ed31760e683e0
LeadSeason <pauli.tiittanen113@gmail.com>
2022-11-18 07:02:40 +0200

    Made some changes

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
 
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("showPhone").addEventListener("click", function (e) {
+        div = document.getElementById("contactShowData");
+        div.innerHTML = '<a rel="noreferrer" target="_blank" class="cta-btn cta-btn--resume">+358 404129151</a>';
+    })
+}); 
