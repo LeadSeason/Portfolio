4c97ecb32df8d059a0955da293e1d4f80daffadf
LeadSeason <Pauli.tiittanen113@gmail.com>
2022-12-14 10:36:00 +0200

    Format

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("showPhone").addEventListener("click", function (e) {
         let div = document.getElementById("contactShowData");
         div.innerHTML = '<a rel="noreferrer" target="_blank" href="tel:+358404129151" class="cta-btn cta-btn--resume">+358 40 412 9151</a>';
