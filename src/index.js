7c27bd94e37659de8662448a55d7fe8870378611
LeadSeason <Pauli.tiittanen113@gmail.com>
2022-12-14 10:30:43 +0200

    Tweaks to phone number

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ initTiltEffect();
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("showPhone").addEventListener("click", function (e) {
         let div = document.getElementById("contactShowData");
-        div.innerHTML = '<a rel="noreferrer" target="_blank" class="cta-btn cta-btn--resume">+358 404129151</a>';
+        div.innerHTML = '<a rel="noreferrer" target="_blank" href="tel:+358404129151" class="cta-btn cta-btn--resume">+358 40 412 9151</a>';
     })
 }); 
